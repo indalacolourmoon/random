01fc1f0d94b4bc761af3bad726d6d228fceb3ccd
indala <indalamohankumar@gmail.com>
2026-04-18 20:14:30 +0530

    init

--- scratch/read_001.py
+++
@@ -1,6 +0,0 @@
-import pdfplumber
-
-pdf_path = 'public/uploads/published/IJITEST-2026-001-published.pdf'
-with pdfplumber.open(pdf_path) as pdf:
-    text = pdf.pages[0].extract_text()
-    print(text)
