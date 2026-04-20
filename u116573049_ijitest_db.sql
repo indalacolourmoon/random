4fc46b7911242112b9535a8f5a6dcd726422592c
indala <indalamohankumar@gmail.com>
2026-04-20 07:51:40 +0530

    init

--- u116573049_ijitest_db.sql
+++ u116573049_ijitest_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 19, 2026 at 06:50 AM
+-- Generation Time: Apr 20, 2026 at 02:11 AM
 -- Server version: 12.3.1-MariaDB-log
 -- PHP Version: 7.2.34
 
